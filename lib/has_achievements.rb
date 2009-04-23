ee40bef74de34fdd9431f0008d2aed797ed52c67
Mark M <yoster84@yahoo.com>
2009-04-22 21:41:26 -0500

    added HookedAchievement class to map achievements in models to their hook methods

--- lib/has_achievements.rb
+++ lib/has_achievements.rb
@@ -5,12 +5,14 @@ module HasAchievements #:nodoc:
   end
   
   
+  
   module HasManyMacro
     def has_achievements(options={})
       include InstanceMethods
       extend ClassMethods
     end
   end # HasManyMacro
+  
   
   
   module ClassMethods
@@ -21,14 +23,39 @@ module HasAchievements #:nodoc:
     
     # Add achievement to class's achievements hash
     def achievement(title, options={})
-      hook = {} # Just a test for now
-      write_inheritable_hash(:achievements, title => hook)
+      hook_method_name   = "#{title}_hook"
+      hooked_achievement = HookedAchievement.new(title, hook_method_name, options)
+      
+      # Todo: Create actual hook method that takes in proc
+      
+      write_inheritable_hash(:achievements, title => hooked_achievement)
     end
   end # ClassMethods
+  
   
   
   module InstanceMethods
     
   end # InstanceMethods
+  
+  
+  
+  # Grabs achievements defined from model and maps to hook method name
+  class HookedAchievement
+    attr_reader :id, :title, :value, :hook_method, :model_class_name
+    
+    def initialize(title, hook_method, options={})
+      if achievement = Achievement.find_by_title(title.to_s.gsub(/_id$/, "").gsub(/_/, " ").capitalize)
+        @id          = achievement.id
+        @title       = achievement.title
+        @value       = achievement.value
+        @hook_method = hook_method
+      else
+        raise "Model has achievement '#{title}' that does not exist in achievements db table. Make sure a achievement with the title of '#{title.to_s.gsub(/_id$/, "").gsub(/_/, " ").capitalize}' exists before defining in model."
+      end
+    end
+  end # AchievementItem
+  
+  
   
 end # HasAchievements
